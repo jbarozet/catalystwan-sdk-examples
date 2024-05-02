1db0cd766cbc8799ba8d8cf4080d5e7b1b97f271
Jean-Marc Barozet <jbarozet@gmail.com>
2024-05-02 16:19:54 +0200

    cleaning code and use class definition for Manager

--- config-groups-raw.py
+++ config-groups-raw.py
@@ -24,81 +24,24 @@
 #
 # =========================================================================
 
-import json
 import os
-from os.path import join
 
 import click
 
-from session import create_session
-
-workdir_base = "data/outputs/"
-
-# feature-profiles: id, type
-profile_id_table = []
-
-# config-group: id, name, number of devices
-config_group_table = []
+from manager import MyManager
 
 
-def create_workdir():
-    """
-    Create output folder structure
-    """
-    # current_datetime = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    # workdir = "data/outputs/" + current_datetime
-    workdir = "data/outputs/"
-    # os.mkdir(workdir)
-    # Check if workdir folder already exists. If yes, then stop backup process
-    workdir = join(os.path.abspath(os.getcwd()), workdir)
-    # if os.path.isdir(workdir):
-    #     exit(f"{workdir} folder is already in use. Please select a different workdir directory.")
-
-    # create config-group folders
-    cg_dir = ["groups", "associated", "values"]
-    for dir in cg_dir:
-        try:
-            os.makedirs(os.path.join(workdir, "config_groups", dir))
-        except OSError:
-            pass
-
-    # Create feature-profiles folders
-    fp_dir = ["cli", "service", "system", "transport", "policy-objects"]
-    for dir in fp_dir:
-        try:
-            os.makedirs(os.path.join(workdir, "feature_profiles", dir))
-        except OSError:
-            pass
-
-    return workdir
-
-
-def save_output(payload, name, type):
-    """
-    Save API json payload into a file
-    """
-
-    match type:
-        case "groups":
-            current_dir = workdir + "/config_groups/groups"
-        case "cli":
-            current_dir = workdir + "/feature_profiles" + "/cli"
-        case "system":
-            current_dir = workdir + "/feature_profiles" + "/system"
-        case "transport":
-            current_dir = workdir + "/feature_profiles" + "/transport"
-        case "service":
-            current_dir = workdir + "/feature_profiles" + "/service"
-        case "policy-object":
-            current_dir = workdir + "/feature_profiles" + "/policy-objects"
-        case _:
-            exit(f"{type} type not supported")
-
-    tmp = name + ".json"
-    filename = join(current_dir, tmp)
-
-    with open(filename, "w") as file:
-        json.dump(payload, file, indent=4)
+def help():
+    print("For Windows Workstation, vManage details must be set via environment variables using below commands")
+    print("set vmanage_host=10.10.1.1")
+    print("set vmanage_port=8443")
+    print("set vmanage_user=admin")
+    print("set vmanage_password=admin")
+    print("For MAC OSX Workstation, vManage details must be set via environment variables using below commands")
+    print("export vmanage_host=10.10.1.1")
+    print("export vmanage_port=8443")
+    print("export vmanage_user=admin")
+    print("export vmanage_password=admin")
 
 
 @click.group()
@@ -108,137 +51,56 @@ def cli():
 
 
 @click.command()
-def list_groups():
-    """
-    List all config-groups with their profiles
-    Save payloads in files
-    """
-
-    # API endpoint
-    url_base = "dataservice/v1/config-group/"
-
-    # Get payload
-    data = session.get(url_base).json()
-
-    print(f"\n~~~ Saving Config Groups in {workdir}\n")
-
-    for key in data:
-        config_group_id = key["id"]
-        config_group_name = key["name"]
-        new_element = [config_group_name, config_group_id, 0]
-        config_group_table.append(new_element)
-
-        print(f"> Config Group ID ❯ {config_group_name}")
-
-        # Get config-group profiles payload
-        url = url_base + config_group_id
-        config_group = session.get(url).json()
-
-        # save all associated feature-profiles in a table
-        for item in config_group["profiles"]:
-            profile_name = item["name"]
-            profile_id = item["id"]
-            profile_type = item["type"]
-            new_element = [profile_id, profile_type]
-            profile_id_table.append(new_element)
-            print(f"  - profile-name: {profile_name} -type: {profile_type}, id: {profile_id}")
-
-        # Save config-group payload in file
-        save_output(config_group, config_group_name, "groups")
+def save_groups():
+    manager.save_groups()
+    manager.save_associated_devices()
+    manager.save_config_group_values()
 
 
 @click.command()
-def list_profiles_summary():
-    """Feature Profiles - Get all profiles"""
-
-    base = "dataservice/v1/feature-profile/sdwan/"
-    data = session.get(base).json()
-    i = 0
-    for item in data:
-        data_formatted = json.dumps(data[i], indent=4)
-        print(data_formatted)
-        i = i + 1
+def save_profiles():
+    manager.save_profiles
 
 
 @click.command()
-def list_profiles():
-    """
-    List Feature Profiles with details and parcels
-    Save payloads in files
-    """
-
-    url_base = "dataservice/v1/feature-profile/sdwan/"
-
-    print("\n~~~ Saving Features Profiles in {workdir}\n")
-
-    # Get list of profiles
-    data = session.get(url_base).json()
-
-    # Get profile details using profile_id
-    for item in data:
-        profile_name = item["profileName"]
-        profile_id = item["profileId"]
-        profile_type = item["profileType"]
-
-        match profile_type:
-            case "system":
-                urlp = url_base + "system/"
-            case "transport":
-                urlp = url_base + "transport/"
-            case "service":
-                urlp = url_base + "service/"
-            case "cli":
-                urlp = url_base + "cli/"
-            case "policy-object":
-                urlp = url_base + "policy-object/"
-            case _:
-                exit(f"{profile_type} type not supported")
-
-        # Get profile_id payload
-        # NOTE: details option has been added in 20.12
-        url = urlp + profile_id + "?details=true"
-        data = session.get(url).json()
-
-        profile_name = data["profileName"]
-        print(f"> Profile Name ❯ {profile_name} - {profile_id} - {profile_type}")
-        save_output(data, profile_name, profile_type)
+def profiles_summary():
+    manager.list_sdwan_profiles_summary()
+    manager.list_sdrouting_profiles_summary()
 
 
 @click.command()
-def list_profiles_categories():
-    """
-    List Feature Profiles per category:
-        - system
-        - transport
-        - service
-        - cli
-        - policy-object
-    """
-
-    categories = [
-        "dataservice/v1/feature-profile/sdwan/system",
-        "dataservice/v1/feature-profile/sdwan/transport",
-        "dataservice/v1/feature-profile/sdwan/service",
-        "dataservice/v1/feature-profile/sdwan/cli",
-        "dataservice/v1/feature-profile/sdwan/policy-object",
-    ]
-
-    for item in categories:
-        data = session.get(item).json()
-        i = 0
-        for item in data:
-            data_formatted = json.dumps(data[i], indent=4)
-            print(data_formatted)
-            i = i + 1
+def profiles_categories():
+    manager.list_profiles_categories()
 
 
-cli.add_command(list_groups)
-cli.add_command(list_profiles)
-cli.add_command(list_profiles_summary)
-cli.add_command(list_profiles_categories)
+@click.command()
+def save_associated_devices():
+    manager.save_associated_devices()
 
-session = create_session()
-workdir = create_workdir()
 
+# Add commands to CLI
+cli.add_command(save_groups)
+cli.add_command(save_profiles)
+cli.add_command(profiles_summary)
+cli.add_command(profiles_categories)
+cli.add_command(save_associated_devices)
+
+# Main
 if __name__ == "__main__":
+
+    url = os.environ.get("vmanage_host")
+    user = os.environ.get("vmanage_user")
+    password = os.environ.get("vmanage_password")
+
+    if url is None or user is None or password is None:
+        help()
+        exit()
+
+    manager = MyManager(url, user, password)
+
+    if not manager.status:
+        exit(1)
+
+    manager.create_workdir()
+
     cli()
