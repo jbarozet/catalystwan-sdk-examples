5b1434ddd4f773457dbb56527e861238c19c6e60
Jean-Marc Barozet <jbarozet@gmail.com>
2024-05-06 17:49:38 +0200

    cleaning and listing only edge devices

--- omp.py
+++ omp.py
@@ -1,10 +1,19 @@
+from catalystwan.dataclasses import Personality
+
 from session import create_session
 
 # Create Session
 session = create_session()
 
+# Get the list of devices
+all_devices = session.api.devices.get()
+
+edges = all_devices.filter(personality=Personality.EDGE)
+
+for item in edges:
+    print(f"~~~ {item.hostname} - {item.local_system_ip}")
+
 # Get OMP peers
-# deviceid = "10.0.0.2"
 deviceid = input("Enter Device ID : ")
 omp_peers = session.api.omp.get_omp_peers(deviceid)
 
