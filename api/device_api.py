e354bc7e8dda84adb4b2c3106deb2db6037032bf
Jean-Marc Barozet <jbarozet@gmail.com>
2024-06-26 10:20:08 +0200

    add a few more examples

--- api/device_api.py
+++ api/device_api.py
@@ -21,7 +21,7 @@ for dev in devices:
     print(f" - {dev.hostname} - Load: {dev.cpu_load} - Board serial: {dev.board_serial} - UUID: {dev.uuid}")
     # print(session.api.device_state.get_system_status(dev.id))
 
-# Filter vmanage devices
+# Filter SD-WAN Controllers
 vsmarts = devices.filter(personality=Personality.VSMART)
 
 print("\n~~~ SD-WAN Controllers")
@@ -31,6 +31,21 @@ for item in vsmarts:
         f" - {item.hostname} - {item.local_system_ip} - Load: {item.cpu_load} - Board serial: {item.board_serial} - UUID: {item.uuid}"
     )
 
+# Filter devices
+edge_cpu = devices.filter(hostname="ciscotme-cloud-fabric-cedge-1").single_or_default().cpu_load
+print("\n~~~ SD-WAN edge1")
+print(f" - Edge CPU load: {edge_cpu}")
+
+# Device Health
+device_health = session.api.dashboard.get_devices_health_overview()
+print("\n~~~ Device Health")
+print(device_health)
+
+# Tunnel Health
+tunnel_health = session.api.dashboard.get_tunnel_health()
+print("\n~~~ Tunnel Health")
+print(tunnel_health)
+
 session.close()
 
-# ---END--
+# ---END---
