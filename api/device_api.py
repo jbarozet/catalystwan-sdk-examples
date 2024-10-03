c5addb32500d5b0816923817a8b543f0b4ff7b47
Jean-Marc Barozet <jbarozet@gmail.com>
2024-10-03 13:09:43 +0200

    update

--- api/device_api.py
+++ api/device_api.py
@@ -12,13 +12,11 @@ session = create_session()
 # Get the list of devices
 devices = session.api.devices.get()
 count = session.api.devices.count_devices(personality=Personality.EDGE)
-
-print(f"count: {count}")
-
+print(f"\nNumber of WAN Edge devices in the fabric: {count}")
 # Display the list of devices
 print("\n~~~ All devices")
 for dev in devices:
-    print(f" - {dev.hostname} - Load: {dev.cpu_load} - Board serial: {dev.board_serial} - UUID: {dev.uuid}")
+    print(f" - {dev.hostname} - Board serial: {dev.board_serial} - UUID: {dev.uuid}")
     # print(session.api.device_state.get_system_status(dev.id))
 
 # Filter SD-WAN Controllers
@@ -27,24 +25,25 @@ vsmarts = devices.filter(personality=Personality.VSMART)
 print("\n~~~ SD-WAN Controllers")
 
 for item in vsmarts:
-    print(
-        f" - {item.hostname} - {item.local_system_ip} - Load: {item.cpu_load} - Board serial: {item.board_serial} - UUID: {item.uuid}"
-    )
+    print(f" - {item.hostname} - {item.local_system_ip} - Board serial: {item.board_serial} - UUID: {item.uuid}")
 
 # Filter devices
-edge_cpu = devices.filter(hostname="ciscotme-cloud-fabric-cedge-1").single_or_default().cpu_load
+edge_cpu = devices.filter(hostname="site1-cedge01").single_or_default().cpu_load
 print("\n~~~ SD-WAN edge1")
 print(f" - Edge CPU load: {edge_cpu}")
 
 # Device Health
 device_health = session.api.dashboard.get_devices_health_overview()
 print("\n~~~ Device Health")
-print(device_health)
+print(f"Good: {device_health.single_or_default().good}")
+print(f"Fair: {device_health.single_or_default().fair}")
+print(f"Poor: {device_health.single_or_default().poor}")
 
 # Tunnel Health
 tunnel_health = session.api.dashboard.get_tunnel_health()
 print("\n~~~ Tunnel Health")
-print(tunnel_health)
+for item in tunnel_health:
+    print(f"{item.name} - score: {item.vqoe_score}")
 
 session.close()
 
