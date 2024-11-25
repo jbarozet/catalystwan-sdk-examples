52f1dc91aedd6d4d3f2e9bf865ab915a57870219
Jean-Marc Barozet <jbarozet@gmail.com>
2024-11-25 10:40:25 +0100

    save all config-groups and profiles under folder cg

--- raw/config_groups_raw.py
+++ raw/config_groups_raw.py
@@ -6,12 +6,12 @@ sys.path.insert(0, "..")
 from utils.session import create_session
 
 # Create payload folder
-path = "./payloads"
-if not os.path.exists(path):
-    os.mkdir(path)
-    print("Folder %s created!" % path)
+data_dir = "./payloads/cg/"
+if not os.path.exists(data_dir):
+    os.mkdir(data_dir)
+    print("Folder %s created!" % data_dir)
 else:
-    print("Folder %s already exists" % path)
+    print("Folder %s already exists" % data_dir)
 
 
 # Create session
@@ -20,18 +20,20 @@ session = create_session()
 # Get list of config groups
 url_base = "dataservice/v1/config-group/"
 data = session.get(url_base).json()
+filename = "".join([data_dir, "1_payload_config_groups.json"])
 
 print("\n~~~ Saving payload in file payload_config_groups.json")
-with open("payloads/payload_config_groups.json", "w") as file:
+with open(filename, "w") as file:
     json.dump(data, file, indent=4)
 
 
 # Get list of feature profiles
 url_base = "dataservice/v1/feature-profile/sdwan/"
 data = session.get(url_base).json()
+filename = "".join([data_dir, "2_payload_feature_profiles.json"])
 
 print("\n~~~ Saving payload in file payload_feature_profiles.json")
-with open("payloads/payload_feature_profiles.json", "w") as file:
+with open(filename, "w") as file:
     json.dump(data, file, indent=4)
 
 # Get feature profile payloads
@@ -42,20 +44,29 @@ for key in data:
     match profile_type:
         case "system":
             urlp = url_base + "system/"
+            url = urlp + profile_id + "?details=true"
+            filename = "".join([data_dir, "3_payload_feature_profile", "_system_", profile_id, ".json"])
         case "transport":
             urlp = url_base + "transport/"
+            url = urlp + profile_id + "?details=true"
+            filename = "".join([data_dir, "4_payload_feature_profile", "_transport_", profile_id, ".json"])
         case "service":
             urlp = url_base + "service/"
+            url = urlp + profile_id + "?details=true"
+            filename = "".join([data_dir, "5_payload_feature_profile", "_service_", profile_id, ".json"])
         case "cli":
             urlp = url_base + "cli/"
+            url = urlp + profile_id + "?details=true"
+            filename = "".join([data_dir, "6_payload_feature_profile", "_cli_", profile_id, ".json"])
         case "policy-object":
             urlp = url_base + "policy-object/"
+            url = urlp + profile_id
+            filename = "".join([data_dir, "7_payload_feature_profile", "_policy_", profile_id, ".json"])
         case _:
             exit(f"{profile_type} type not supported")
 
-    url = urlp + profile_id + "?details=true"
     data = session.get(url).json()
-    filename = "".join(["payloads/", "payload_feature_profile_", profile_id, ".json"])
+
     print(f"\n~~~ Saving {profile_id} payload details in {filename}")
     with open(filename, "w") as file:
         json.dump(data, file, indent=4)
