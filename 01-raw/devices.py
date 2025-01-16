6647a6004f089e8e60b69985c48ec97e38fe333a
Jean-Marc Barozet <jbarozet@gmail.com>
2025-01-16 09:55:19 +0100

    updates and cleaning

--- 01-raw/devices.py
+++ 01-raw/devices.py
@@ -10,9 +10,29 @@ sys.path.insert(0, "..")
 from utils.session import create_session
 
 
-# ----------------------------------------------------------------------------------------------------
-# Click CLI
-# ----------------------------------------------------------------------------------------------------
+def save_json(payload: str, data: str):
+    """Save json response payload to a file"""
+
+    data_dir = "./payloads/"
+    filename_data = "".join([data_dir, "payload_devices_data.json"])
+    filename_payload = "".join([data_dir, "payload_devices_all.json"])
+
+    # Create payload folder
+    if not os.path.exists(data_dir):
+        os.mkdir(data_dir)
+        print("~~~ Folder %s created!" % data_dir)
+    else:
+        print("~~~ Folder %s already exists" % data_dir)
+
+    # Dump entire payload to file
+    print(f"~~~ Saving full payload in {filename_payload}")
+    with open(filename_payload, "w") as file:
+        json.dump(payload, file, indent=4)
+
+    # Dump payload data (device list) to file
+    print(f"~~~ Saving data payload in {filename_data}")
+    with open(filename_data, "w") as file:
+        json.dump(data, file, indent=4)
 
 
 @click.group()
@@ -22,68 +42,35 @@ def cli():
 
 
 @click.command()
-def list_table():
-    """Retrieve the list of devices (wan edges)
-    Example command: python devices.py list-table
+def list_all():
+    """Retrieve the list of all devices (wan edges)
+    Example command: python devices.py list-all
     """
 
     with create_session() as session:
-        url_base = "dataservice/system/device/vedges"
-        response = session.get(url_base)
+        url = "dataservice/system/device/vedges"
+        response = session.get(url)
 
-    if response.status_code == 200:
-        data = response.json()["data"]
-        app_headers = ["UUID", "Model", "Certificate"]
-    else:
-        click.echo("Failed to get device list " + str(response.text))
-        exit()
+        if response.status_code == 200:
+            payload = response.json()
+            data = response.json()["data"]
+            save_json(payload, data)
+            headers = ["UUID", "Model", "Certificate"]
+        else:
+            click.echo("Failed to get device list " + str(response.text))
+            exit()
 
-    table = list()
+        table = list()
 
-    for item in data:
-        tr = [
-            item["uuid"],
-            item["deviceModel"],
-            item["vedgeCertificateState"],
-        ]
-        table.append(tr)
+        for item in data:
+            tr = [
+                item["uuid"],
+                item["deviceModel"],
+                item["vedgeCertificateState"],
+            ]
+            table.append(tr)
 
-    click.echo(tabulate.tabulate(table, app_headers, tablefmt="fancy_grid"))
-
-
-@click.command()
-def list_json():
-    """Retrieve the list of devices (wan edges) and save json payload in file
-    Example command: python devices.py list-json
-    """
-
-    # Create payload folder
-    data_dir = "./payloads/"
-    filename_data = "".join([data_dir, "payload_devices_data.json"])
-    filename_payload = "".join([data_dir, "payload_devices_all.json"])
-
-    if not os.path.exists(data_dir):
-        os.mkdir(data_dir)
-        print("\n~~~ Folder %s created!" % data_dir)
-    else:
-        print("\n~~~ Folder %s already exists" % data_dir)
-
-    with create_session() as session:
-        url_base = "dataservice/system/device/vedges"
-        payload = session.get(url_base).json()
-
-        # Get rid of header section and only keep data
-        devices = payload["data"]
-
-        # Dump entire payload to file
-        print(f"\n~~~ Saving payload in {filename_payload}")
-        with open(filename_payload, "w") as file:
-            json.dump(payload, file, indent=4)
-
-        # Dump payload data (device list) to file
-        print(f"\n~~~ Saving payload in {filename_data}")
-        with open(filename_data, "w") as file:
-            json.dump(devices, file, indent=4)
+        click.echo(tabulate.tabulate(table, headers, tablefmt="fancy_grid"))
 
 
 @click.command()
@@ -96,67 +83,30 @@ def list_deployed():
 
         # Get list of devices
         # url_base = "dataservice/device/vedgeinventory/summary"
-        url_base = "/dataservice/device/vedgeinventory/detail?status=deployed"
-        response = session.get(url_base)
+        url = "/dataservice/device/vedgeinventory/detail?status=deployed"
+        response = session.get(url)
 
-    if response.status_code == 200:
-        data = response.json()["data"]
-        app_headers = ["UUID", "host-name", "site-id", "system-ip"]
-    else:
-        click.echo("Failed to get device list " + str(response.text))
-        exit()
-
-    table = list()
-
-    for item in data:
-        tr = [
-            item["chasisNumber"],
-            item["host-name"],
-            item["site-id"],
-            item["system-ip"],
-        ]
-        table.append(tr)
-
-    click.echo(tabulate.tabulate(table, app_headers, tablefmt="fancy_grid"))
-
-
-@click.command()
-def list_deployed_json():
-    """Retrieve the list of devices (wan edges) that are deployed
-    and save json payload in a file
-    Example command: python devices.py list-deployed-json
-    """
-
-    with create_session() as session:
-
-        # Create payload folder
-        data_dir = "./payloads/"
-        filename_data = "".join([data_dir, "payload_devices_deployed_data.json"])
-        filename_payload = "".join([data_dir, "payload_devices_deployed_all.json"])
-
-        if not os.path.exists(data_dir):
-            os.mkdir(data_dir)
-            print("\n~~~ Folder %s created!" % data_dir)
+        if response.status_code == 200:
+            payload = response.json()
+            data = response.json()["data"]
+            save_json(payload, data)
+            headers = ["UUID", "host-name", "site-id", "system-ip"]
         else:
-            print("\n~~~ Folder %s already exists" % data_dir)
+            click.echo("Failed to get device list " + str(response.text))
+            exit()
 
-        # Get list of devices
-        # url_base = "dataservice/device/vedgeinventory/summary"
-        url_base = "/dataservice/device/vedgeinventory/detail?status=deployed"
-        payload = session.get(url_base).json()
+        table = list()
 
-        # Get rid of header section and only keep data
-        data = payload["data"]
+        for item in data:
+            tr = [
+                item["chasisNumber"],
+                item["host-name"],
+                item["site-id"],
+                item["system-ip"],
+            ]
+            table.append(tr)
 
-        # Dump entire payload to file
-        print(f"\n~~~ Saving payload in {filename_payload}")
-        with open(filename_payload, "w") as file:
-            json.dump(payload, file, indent=4)
-
-        # Dump payload data (device list) to file
-        print(f"\n~~~ Saving payload in {filename_data}")
-        with open(filename_data, "w") as file:
-            json.dump(data, file, indent=4)
+        click.echo(tabulate.tabulate(table, headers, tablefmt="fancy_grid"))
 
 
 @click.command()
@@ -172,7 +122,9 @@ def list_by_ip():
         response = session.get(url)
 
         if response.status_code == 200:
+            payload = response.json()
             data = response.json()["data"]
+            save_json(payload, data)
         else:
             click.echo("Failed to get device " + str(response.text))
             exit()
@@ -189,16 +141,16 @@ def list_by_ip():
                 item["vmanageConnectionState"],
             ]
 
-        print("\nDevice Information:")
-        print("------------------")
-        print("Device name: ", tr[5])
-        print("Device IP: ", tr[4])
-        print("UUID: ", tr[1])
-        print("Device Model: ", tr[2])
-        print("vManage Connection State: ", tr[7])
-        print("Certificate state: ", tr[3])
-        print("Version: ", tr[6])
-        print("Config status: ", tr[0])
+            print("\nDevice Information:")
+            print("------------------")
+            print("Device name: ", tr[5])
+            print("Device IP: ", tr[4])
+            print("UUID: ", tr[1])
+            print("Device Model: ", tr[2])
+            print("vManage Connection State: ", tr[7])
+            print("Certificate state: ", tr[3])
+            print("Version: ", tr[6])
+            print("Config status: ", tr[0])
 
 
 @click.command()
@@ -218,9 +170,11 @@ def config():
         if response.status_code == 200:
             payload = response.json()
             running_config = payload["config"]
+            save_json(payload, running_config)
         else:
             click.echo("Failed to get device configuration " + str(response.text))
             exit()
+
         print(running_config)
 
 
@@ -228,11 +182,9 @@ def config():
 # Run commands
 # ----------------------------------------------------------------------------------------------------
 
-cli.add_command(list_table)
-cli.add_command(list_json)
-cli.add_command(list_by_ip)
+cli.add_command(list_all)
 cli.add_command(list_deployed)
-cli.add_command(list_deployed_json)
+cli.add_command(list_by_ip)
 cli.add_command(config)
 
 if __name__ == "__main__":
